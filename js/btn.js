dd23756fdbdf94580ae9534e9770889b92e6efd5
AraceliDC <agdc.105@gmail.com>
2024-01-29 22:50:34 -0300

    landing ready

--- js/btn.js
+++ js/btn.js
@@ -1,8 +1,23 @@
 let btn1 = document.getElementById("btn1")
 let btn2 = document.getElementById("btn2")
 let btn3 = document.getElementById("btn3")
+let outline = document.getElementsByClassName("outline")
  
-btn1.addEventListener("click", showBtn())
+btn1.addEventListener("click", function(){
+    btn2.classList.toggle("open")
+    btn3.classList.toggle("open")
+})
+
+
+
+
+// btn1.addEventListener("click", function(){
+//     container1.classlist.toggle('open');
+// })
+
+//btn2.classList.contains("active") && btn3.classList.contains("active") == true 
+
+/*btn1.addEventListener("click", showBtn())
 function showBtn(){
     if (btn2.style.display=='none' && btn3.style.display=='none') {
         btn2.style.display ='block';
@@ -14,8 +29,5 @@ function showBtn(){
         btn3.classList.remove("active");
         btn2.style.display ='none';
         btn3.style.display ='none';
-    }
-}
-
-
-//btn2.classList.contains("active") && btn3.classList.contains("active") == true 
+    }  
+}*/
