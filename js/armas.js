1475938d3b95f14bd97dd9f86102c90bb18ec9a4
AraceliDC <agdc.105@gmail.com>
2024-02-12 22:57:20 -0300

    intento mucho

--- js/armas.js
+++ js/armas.js
@@ -1,54 +1,18 @@
-let nameWeapon = document.querySelector('.btn-content')
-let weaponContainer = document.getElementById('despliegue-armas')
 
-// const fetchWeapon = async () =>{
-    
-//     try {
-//         const url = 'https://pokeapi.co/api/v2/pokemon/';
-//         const response = await axios.get(url)
-//         weapon = response.data
+
+const fetchWeapon = async() => {
+   let weapon = document.getElementById('weapons').value
+   let img = document.getElementById('weapon-image')
+    try {
+        const url = `https://valorant-api.com/v1/weapons/${weapon}`;
+        const response = await axios.get(url)
+        const imgWeapon = response.data.data.displayIcon
+        console.log(imgWeapon)
         
-//         console.log(weapon)
-//         // arrayMap()
-//         // console.group()
-//         // console.log(responseNW)
-//         // console.log("responseNW")
-//         // console.group()
-//         // weaponSlide(response.data.data);
-//     } catch (error) {
-//         console.log('Ha fallado', error)
-//     }
-// }
-
-// fetchWeapon();
-
-// nameWeapon.addEventListener('click', weaponSlide())
-
-// function weaponSlide(weapon){
-//     if (nameWeapon == true){
-//         p= document.createElement('p')
-//         p.textContent = weapon.data;
-//         weaponContainer.appendChild(p)
-//     }
-// }
-// weaponSlide();
-
-// function arrayMap(responseNW){
-//     for (let index = 0; index < responseNW.length; index++) {
-//         const weapon = responseNW[index];
-//         weaponName = weapon[index].displayName;
-//     }
-//     return weaponName;
-// }
-
-
-
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-nameWeapon.addEventListener('click', function(){
-    axios.get(url, {responseType: 'JSON'})
-})
-.then(function(response){
-    if (response.status == 200) {
-        
+        img.src = imgWeapon
+    } catch (error) {
+        console.log('ha fallado', error)
     }
-})
+}
+
+
